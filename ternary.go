ca7d3b06f338dbc37429d990a143911f41ad0a34
Jaco Labuschagne <zodimo@gmail.com>
2025-11-22 08:54:35 +0200

    add func types

--- ternary.go
+++ ternary.go
@@ -2,12 +2,16 @@ package ternary
 
 import "github.com/zodimo/go-lazy"
 
+type TernaryFunc[T any] func(condition bool, value1 T, value2 T) T
+
 func Ternary[T any](condition bool, value1 T, value2 T) T {
 	if condition {
 		return value1
 	}
 	return value2
 }
+
+type TernaryLazyFunc[T any] func(condition lazy.Value[bool], value1 lazy.Value[T], value2 lazy.Value[T]) lazy.Value[T]
 
 func TernaryLazy[T any](condition lazy.Value[bool], value1 lazy.Value[T], value2 lazy.Value[T]) lazy.Value[T] {
 	return lazy.NewLazy(func() T {
